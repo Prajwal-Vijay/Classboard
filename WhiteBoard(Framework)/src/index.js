26cc51366f5af8e947342a103b398a884342c7d5
Prajwal Vijay <prajvi.10@gmail.com>
2021-02-25 17:41:35 +0530

    version 1.0.0.5

--- WhiteBoard(Framework)/src/index.js
+++ WhiteBoard(Framework)/src/index.js
@@ -538,16 +538,47 @@ function init(canvas) {
 	$(document).bind("contextmenu", function (event) {
 		// Avoid the real one
 		event.preventDefault();
-
-		// Show contextmenu
+		//Get window size:
+		var winWidth = $(document).width();
+		var winHeight = $(document).height();
+		//Get pointer position:
+		var posX = event.pageX;
+		var posY = event.pageY;
+		//Get contextmenu size:
+		var menuWidth = $(".custom-menu").width();
+		var menuHeight = $(".custom-menu").height();
+		//Security margin:
+		var secMargin = 10;
+		//Prevent page overflow:
+		if (
+			posX + menuWidth + secMargin >= winWidth &&
+			posY + menuHeight + secMargin >= winHeight
+		) {
+			//Case 1: right-bottom overflow:
+			posLeft = posX - menuWidth - secMargin + "px";
+			posTop = posY - menuHeight - secMargin + "px";
+		} else if (posX + menuWidth + secMargin >= winWidth) {
+			//Case 2: right overflow:
+			posLeft = posX - menuWidth - secMargin + "px";
+			posTop = posY + secMargin + "px";
+		} else if (posY + menuHeight + secMargin >= winHeight) {
+			//Case 3: bottom overflow:
+			posLeft = posX + secMargin + "px";
+			posTop = posY - menuHeight - secMargin + "px";
+		} else {
+			//Case 4: default values:
+			posLeft = posX + secMargin + "px";
+			posTop = posY + secMargin + "px";
+		}
+		//Display contextmenu:
 		$(".custom-menu")
-			.finish()
-			.toggle(100)
-			// In the right position (the mouse)
 			.css({
-				top: event.pageY + "px",
-				left: event.pageX + "px",
-			});
+				left: posLeft,
+				top: posTop,
+			})
+			.show();
+		//Prevent browser default contextmenu.
+		return false;
 	});
 
 	// If the document is clicked somewhere
